09a6d95ce95532195baf889dabbbb3c23f7bd93b
Nicky J <njord029@uottawa.ca>
2017-12-03 18:57:36 -0500

    A4-Q3-PHP
    Ready for submission

--- q3.php
+++ q3.php
@@ -1,22 +1,31 @@
 <?php
-$iters = 1000000;
-$radius = 1.00;
-$radiusSquared = $radius * $radius;
-$count = 0;
+//CSI3130 - Assignment4 - Part1
+//Question 3: Estimate the value of pi in PHP using random points
 
-for($i = 0; $i < $iters; $i++){
-    $x = rand(0,1);
-    $y = rand(0,1);
+$iters         = 1000000.0; //Number of iterations to run
+$radius        = 1.0;
+$count         = 0; //Keep track of the number of points in the circle
+
+for ($i = 0; $i < $iters; $i++) {
+    $x = rand(0, 1); //Generate random number between 0 and 1
+    $y = rand(0, 1); //Generate random number between 0 and 1
 
     $coords = ($x * $x) + ($y * $y);
-
-    if($coords <= $radiusSquared){
+    #If coordinates are within circle, increment count
+    if ($coords <= ($radius * $radius)) {
         $count++;
     }
 }
-$pi = ($count * 4)/$iters;
+#Estimate pi as the count within the circle divided by the count in the square(which is all points, so # of iterations)
+$pi = ($count * 4.0) / $iters;
 
-echo "The number of points in the square is: "; echo $iters; echo "\n";
-echo "The number of points inside the circle is: "; echo $count; echo "\n";
-echo "The estimatation of pi is: "; echo $pi; echo "\n";
+echo "The number of points in the square is: ";
+echo $iters;
+echo "\n";
+echo "The number of points inside the circle is: ";
+echo $count;
+echo "\n";
+echo "The estimatation of pi is: ";
+echo $pi;
+echo "\n";
 ?>
