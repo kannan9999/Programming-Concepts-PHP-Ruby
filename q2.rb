ea0f3c139a895e577a8cafab9c4b010a8a7f80ff
Nicky J <njord029@uottawa.ca>
2017-12-03 17:58:10 -0500

    A4-Q2-Ruby
    Ready to be submitted

--- q2.rb
+++ q2.rb
@@ -1,6 +1,10 @@
-class Queens
-  @@board = Array.new(8){Array.new(8, 0)} #create 2d array of 0s
+#CSI3130 - Assignment4 - Part1
+#Question 2: Queens Placement in Ruby
 
+class Queens
+  @@board = Array.new(8){Array.new(8, 0)} #Create 2d array of 0s of size 8x8
+
+  #Method to output the board on the screen
   def print_board
     puts "________________________"
     for i in 0..7
@@ -14,15 +18,15 @@ class Queens
       puts
     end
   end
-
+  #Method which places 1 on board for queen or 0 for an empty place
   def place_queens(numQueens, row)
       for col in 0..7
         if(isValid(row, col))
-          @@board[row][col] = 1
+          @@board[row][col] = 1 #If this cell isValid, place a queen
           if(numQueens == 7 || self.place_queens(numQueens + 1, row + 1))
-            return true;
+            return true; #If this is the last queen
           end
-          @@board[row][col] = 0;
+          @@board[row][col] = 0; #Else we will backtrack and replace this queen so set to empty place
         end
       end
         return false;
@@ -30,22 +34,26 @@ class Queens
 
   def isValid(x, y)
     for i in 0..x-1
+      #Check if another queen in same column
       if(@@board[i][y] == 1)
         return false
       end
-      tx = x - 1 - i
-      ty = y - 1 - i
-        if(ty >= 0 && @@board[tx][ty] == 1)
+      #Check if there is another queen in the diagonals
+      dx = x - 1 - i
+      dy = y - 1 - i
+      #Check left to right diagonal \
+        if(dy >= 0 && @@board[dx][dy] == 1)
           return false
         end
-        ty = y + 1 + i;
-          if(ty < 8 && @@board[tx][ty] == 1)
+      #Check right to left diagonal /
+        dy = y + 1 + i;
+          if(dy < 8 && @@board[dx][dy] == 1)
             return false
           end
     end
       return true
   end
 end
-queensObj = Queens.new
-queensObj.place_queens(0, 0)
-queensObj.print_board()
+queensObj = Queens.new #Create new queen object
+queensObj.place_queens(0, 0) #Place queens
+queensObj.print_board() #Display the board
